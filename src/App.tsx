fbdea29129d50c4dfafe25d614c0a30f4243e9a2
Momina Babar <mominababar97@gmail.com>
2025-05-20 23:48:34 +0500

    add files

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+
+import { useReactToPrint } from 'react-to-print';
+import './App.css'
+import React, { useCallback, useRef, useState } from 'react';
+
+const RemoteComponent = React.lazy(() => import('remoteComponent/RemoteComponent'));
+
+function App() {
+
+  const componentRef = useRef(null);
+  const [load, setLaod] = useState(false);
+  const handleAfterPrint = useCallback(() => {
+    console.log("Certificate Printed.");
+  }, []);
+
+  const handleBeforePrint = useCallback(() => {
+    console.log("Printing Certificate...");
+    return Promise.resolve();
+  }, []);
+
+  const printFn = useReactToPrint({
+    contentRef: componentRef,
+    documentTitle: `root`,
+    onAfterPrint: handleAfterPrint,
+    onBeforePrint: handleBeforePrint,
+  });
+
+  return (
+    <>
+      <h1 ref={componentRef}>Root Component</h1>
+      <p className="read-the-docs">
+        Root component content
+      </p>
+      <button onClick={printFn}>print</button>
+      <button onClick={()=>{
+        setLaod(true);
+      }}>load remote</button>
+
+      {load && <RemoteComponent/>}
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,85 @@
+import { defineConfig } from "vite";
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import * as dotenv from "dotenv";
+import * as dotenvExpand from "dotenv-expand";
+
+dotenvExpand.expand(dotenv.config());
+
+const remotesAppDev = {
+  remoteComponent: process.env.VITE_REMOTECOMPONENT_LINK,
+};
+
+
+const remotesAppProd = {
+  remoteComponent: process.env.VITE_REMOTECOMPONENT_LINK_PROD,
+};
+
+const remotesApp = process.env.NODE_ENV == "production"
+  ? remotesAppProd
+  : remotesAppDev;
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "app",
+      remotes: {
+        ...remotesApp,
+        from: "webpack",
+        format: "esm",
+      },
+      shared: {
+        'react': {
+          import: true,
+          //singleton: true,
+          requiredVersion: '18.3.1',
+        },
+        'react-dom': {
+          import: true,
+          //singleton: true,
+          requiredVersion: '18.3.1',
+        },
+      },
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        format: "esm",
+        minifyInternalExports: false,
+      },
+      plugins: [
+        // visualizer({
+        //   open: true, // Open the report in the browser automatically
+        //   filename: 'bundle-analysis.html', // Output file
+        //   gzipSize: true, // Display gzip sizes
+        //   brotliSize: true, // Display Brotli sizes
+        // }),
+      ],
+    },
+  },
+  output: {
+    libraryTarget: "system",
+  },
+  base: process.env.NODE_ENV == 'production' ? process.env.VITE_BASE_URL_PROD : process.env.VITE_BASE_URL,
+  devServer: {
+    allowedHosts: "all",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
+    proxy: {
+      "*": {
+        changeOrigin: true,
+        cookieDomainRewrite: "localhost",
+      },
+    },
+  },
+});
